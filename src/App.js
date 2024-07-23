ba83dec5c1f823f3cf0d1c24f223ade6491ed549
viraj deshmukh <128155984+VirajDeshmukh2407@users.noreply.github.com>
2024-07-23 18:09:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,3 @@
-// import React, { useState } from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import "./App.css";
-// import Header from "./pages/Header";
-// import "./pages/Header.css";
-// import Content from "./pages/content";
-// import Bookmarks from "./components/Bookmarks";
-// import Categories from "./components/Categories";
-// function App() {
-//   const [category, setCategory] = useState("general");
-
-//   const handleCategoryClick = (category) => {
-//     setCategory(category);
-//   };
-
-//   return (
-//     <Router>
-//       <div className="container-fluid">
-//         <Header onCategoryClick={handleCategoryClick} />
-//         <div className="row">
-//           <Routes>
-//             <Route path="/" element={<Content category={category} />} />
-//             <Route path="/bookmarks" element={<Bookmarks />} />
-//             <Route
-//               path="/technology"
-//               element={<Categories category={category} />}
-//             />
-//           </Routes>
-//         </div>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
